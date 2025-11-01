18b765bbf23fe832c8925f30c03296c6ed2854cf
taplasz <tamas.plaszko@bio.ku.dk>
2025-11-01 16:57:59 +0100

    added neovim, removed doom & helix

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,13 @@
 local wezterm = require("wezterm")
 
-local config = {}
+local config = wezterm.config_builder()
 
 config.colors = require("cyberdream-light")
--- config.color_scheme = "tokyonight_night"
+-- config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("RobotoMono Nerd Font")
 config.font_size = 12
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+config.default_prog = { "fish", "-l" }
 
 return config
